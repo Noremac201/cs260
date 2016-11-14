b20c3220ec5dc02923fb06ce6e587334e0a947f9
Cameron Moberg <cjmoberg@gmail.com>
2016-11-14 15:51:16 -0600

    Update TCPServer.java

--- networking/TCPServer.java
+++ networking/TCPServer.java
@@ -1,62 +1,89 @@
-package inClass.inClassFri;
+import com.sun.corba.se.spi.activation.Server;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPServer {
-    private final int bufferSize = 512 * 1024;
-    private ServerSocket serverSocket = null;
-    private Socket socket = null;
-    private DataInputStream inStream = null;
-    private DataOutputStream outStream = null;
 
-    public TCPServer() {
-
+    public TCPServer() throws Exception {
+        FilePartitioner f = new FilePartitioner("input.txt", 5);
+        f.createPartitions();
+        doYaThang();
     }
 
-    public static void main(String[] args) throws IOException {
-        TCPServer server = new TCPServer();
-        server.createSocket();
-        server.sendFile();
+    public void doYaThang() throws Exception {
+        ServerThread t0 = new ServerThread(3333, "out0.txt");
+        ServerThread t1 = new ServerThread(3334, "out1.txt");
+        ServerThread t2 = new ServerThread(3335, "out2.txt");
+        ServerThread t3 = new ServerThread(3336, "out3.txt");
+        ServerThread t4 = new ServerThread(3337, "out4.txt");
+
+        t0.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 
-    public void createSocket() {
-        try {
-            serverSocket = new ServerSocket(4444);
-            socket = serverSocket.accept();
+    public class ServerThread extends Thread {
+        private final int bufferSize = 512 * 1024;
+        private ServerSocket serverSocket = null;
+        private Socket socket = null;
+        private DataInputStream inStream = null;
+        private DataOutputStream outStream = null;
+        private int[] ports = new int[5];
 
-            inStream = new DataInputStream(socket.getInputStream());
-            outStream = new DataOutputStream(socket.getOutputStream());
-            System.out.println("Connected");
+        int port;
+        String fileName;
 
-        } catch (IOException io) {
-            io.printStackTrace();
+        public ServerThread(int port, String fileName) {
+            this.port = port;
+            this.fileName = fileName;
         }
-    }
 
-    public void sendFile() {
-        try {
-            File file = new File("Big_Data.zip");
-            FileInputStream fileInput = new FileInputStream(file);
-            int fileSize = (int) file.length();
+        public void run() {
+            createSocket();
+            sendFile();
+        }
 
-            byte[] data = new byte[(int) fileSize];
+        public void createSocket() {
+            try {
+                serverSocket = new ServerSocket(port);
+                socket = serverSocket.accept();
 
-            fileInput.read(data);
+                inStream = new DataInputStream(socket.getInputStream());
+                outStream = new DataOutputStream(socket.getOutputStream());
+                System.out.println("Connected");
 
-            outStream.writeInt(fileSize);
-            outStream.flush();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
 
-            outStream.write(data);
-            outStream.flush();
-            fileInput.close();
-            serverSocket.close();
-            socket.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public void sendFile() {
+            try {
+                File file = new File(fileName);
+                FileInputStream fileInput = new FileInputStream(file);
+                int fileSize = (int) file.length();
+
+                byte[] data = new byte[(int) fileSize];
+
+                fileInput.read(data);
+
+                outStream.writeInt(fileSize);
+                outStream.flush();
+
+                outStream.write(data);
+                outStream.flush();
+                fileInput.close();
+                serverSocket.close();
+                socket.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
