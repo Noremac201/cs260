0c4bde1dd8ac9bb840a4ad28ce382364876068fa
Cameron Moberg <cjmoberg@gmail.com>
2016-11-14 15:51:07 -0600

    Update TCPClient.java

--- networking/TCPClient.java
+++ networking/TCPClient.java
@@ -1,56 +1,91 @@
-package inClass.inClassFri;
-
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.channels.FileChannel;
 
 public class TCPClient {
-    private Socket socket = null;
-    private DataInputStream inStream = null;
-    private DataOutputStream outStream = null;
+    private int counter = 0;
 
-    public TCPClient() {
-
+    public TCPClient() throws Exception {
+        doYaThang();
     }
 
-    public static void main(String[] args) throws Exception {
-        TCPClient client = new TCPClient();
-        client.createSocket();
-        client.recieveFile();
-        /*myChatClient.createReadThread();
-        myChatClient.createWriteThread();*/
+    public void doYaThang() throws Exception {
+        ClientThread t0 = new ClientThread(3333, 0);
+        ClientThread t1 = new ClientThread(3334, 1);
+        ClientThread t2 = new ClientThread(3335, 2);
+        ClientThread t3 = new ClientThread(3336, 3);
+        ClientThread t4 = new ClientThread(3337, 4);
+
+        t0.start();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 
-    public void createSocket() {
-        try {
-            socket = new Socket("localHost", 4444);
-            System.out.println("Connected");
-            inStream = new DataInputStream(socket.getInputStream());
-            outStream = new DataOutputStream(socket.getOutputStream());
+    public class ClientThread extends Thread {
+        private Socket socket = null;
+        private DataInputStream inStream = null;
 
-        } catch (UnknownHostException u) {
-            u.printStackTrace();
-        } catch (IOException io) {
-            io.printStackTrace();
+        private int port;
+        private int fileNum;
+
+        public ClientThread(int port, int fileNum) {
+            this.port = port;
+            this.fileNum = fileNum;
         }
-    }
 
-    public void recieveFile() {
-        try {
-            int fileSize = inStream.readInt();
-            byte data[] = new byte[fileSize];
-            FileOutputStream fileOut = new FileOutputStream("download.zip", true);
+        public void run() {
+            createSocket(port);
+            receiveFile("part" + fileNum + ".txt");
+            try {
+                mergePart();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-            int totalBytes = inStream.read(data, 0, fileSize);
-            fileOut.write(data);
-            fileOut.flush();
-            System.out.println("Filesize is " + fileSize + ", bytes read: " + totalBytes);
-            socket.close();
-            inStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public void createSocket(int port) {
+            try {
+                socket = new Socket("localHost", port);
+                System.out.println("Connected");
+                inStream = new DataInputStream(socket.getInputStream());
+
+            } catch (UnknownHostException u) {
+                u.printStackTrace();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
+        public void receiveFile(String fileName) {
+            try {
+                int fileSize = inStream.readInt();
+                byte data[] = new byte[fileSize];
+                FileOutputStream fileOut = new FileOutputStream(fileName);
+
+                int totalBytes = inStream.read(data, 0, fileSize);
+                fileOut.write(data);
+                fileOut.flush();
+                System.out.println("Filesize is " + fileSize + ", bytes read: " + totalBytes);
+                socket.close();
+                inStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void mergePart() throws IOException {
+            FileChannel in = new FileInputStream(new File("part" + fileNum + ".txt")).getChannel();
+            FileChannel out = new FileOutputStream("final.txt", true).getChannel();
+            while (fileNum != counter) {
+                System.out.println("Waiting to write to file...");
+            }
+            out.transferFrom(in, 0, in.size()); //copies text from one file to another
+            counter++;
         }
     }
 }
